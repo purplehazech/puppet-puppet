91d6090ae752a834fdd22eab4c7be529617f16c3
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-05 15:13:33 +0200

    add url param to puppet::couch

--- manifests/couch.pp
+++ manifests/couch.pp
@@ -2,8 +2,15 @@
 #
 # Installs the couchrest gem and a simple exec node terminus for grabbing
 # data from the couch.
+#
+# === Parameters
 # 
-class puppet::couch {
+# [*url*]
+#  full path to couch endpoint containg node definitions complete with auth info and everything.
+# 
+class puppet::couch (
+  $url
+) {
   package { 'couchrest':
     provider => 'gem'
   }
