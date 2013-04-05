c905144110e1f37bff1710123f73b3ad47afdcd0
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-05 14:59:09 +0200

    implement missing features in couch enc
    mainly some workaround to get clean yaml from json

--- files/couch-enc.rb
+++ files/couch-enc.rb
@@ -8,6 +8,15 @@ data = db.get(ARGV[1])
 
 data['classes'].each do |key,puppetClass|
   data['classes'][key] = nil unless not puppetClass.empty?
+
+  puppetClass.each do |valKey,valVal|
+    data['classes'][key][valKey] = false unless valVal != "false"
+    data['classes'][key][valKey] = true unless valVal != "true"
+  end
+end
+data['parameters'].each do |key,param|
+  data['parameters'][key] = false unless param != "false"
+  data['parameters'][key] = true  unless param != "true"
 end
 
 node = {
