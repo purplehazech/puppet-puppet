97c62876d8558cfc9a3332aee63566e3dc1ea583
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-05 14:34:21 +0200

    simplify storeconfig

--- manifests/storeconfig.pp
+++ manifests/storeconfig.pp
@@ -1,26 +1,12 @@
 # == Class: puppet::storeconfig
 #
-# install routes.yaml and delegate to actual storeconfig implementation
+# install routes.yaml 
 #
-class puppet::storeconfig inherits puppet::params {
-  case $puppet_storeconfig_provider {
-    'puppetdb'     : {
-      $puppet_storeconfig_install = true
-      include puppet::puppetdb
-    }
-    'activerecord' : {
-      $puppet_storeconfig_install = false
-      include activerecord
-    }
-    default        : {
-      $puppet_storeconfig_install = false
-    }
-  }
+class puppet::storeconfig {
 
-  if $puppet_storeconfig_install {
-    file { '/etc/puppet/routes.yaml':
-      content => template('puppet/routes.yaml.erb'),
-      notify  => Service['puppetmaster']
-    }
+  file { '/etc/puppet/routes.yaml':
+    content => template('puppet/routes.yaml.erb'),
+    before  => Class['activerecord'],
+    notify  => Service['puppetmaster']
   }
 }
