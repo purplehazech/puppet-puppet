6cfbec4c2e594b99745283d0050c3a2b88a620b9
Lucas Bickel <hairmare@purplehaze.ch>
2013-06-11 22:20:06 +0200

    use augeas and drop windows support
    I might reconsider the windows thing later when i start adding more
    archs again, for now it has to go because it is holding me back.
    The machines windows was on have long stopped reporting to the
    master anyway.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,35 +9,27 @@
 #   toggle installation of puppet packages
 #
 class puppet (
-  $master = 'puppet',
-  $install = true
+  $master     = 'puppet',
+  $install    = true,
+  $pluginsync = true
 ) {
   
-  case $::operatingsystem {
-    windows: {
-      $puppet_pluginsync    = false
-      $conf_file     = 'C:\Dokumente und Einstellungen\All Users\Anwendungsdaten\PuppetLabs\puppet\etc\puppet.conf'
-      $conf_template = 'puppet.conf.win.erb'
-    }
-    default: {
-      include puppet::gentoo
-      $puppet_pluginsync    = false
-      $conf_file     = '/etc/puppet/puppet.conf'
-      $conf_template = 'puppet.conf.erb'
-    }
-  }
+  include puppet::gentoo
+  $puppet_pluginsync    = false
+  $conf_file     = '/etc/puppet/puppet.conf'
   
-  # @todo also change in win tpl
-  $pluginsync = $puppet_pluginsync
-
   package { 'puppet': 
     ensure => installed,
-    before => File[$conf_file]
+    before => Augeas['puppet-config']
   }
 
-  file { $conf_file:
-    content => template("puppet/${conf_template}"),
-    notify  => Service['puppet']
+  augeas { 'puppet-config':
+    context => '/files/etc/puppet/puppet.conf',
+    changes => [
+      'set agent/report true',
+      "set agent/server ${master}",
+      "set agent/pluginsync ${pluginsync}",
+    ]
   }
 
   service { 'puppet': 
